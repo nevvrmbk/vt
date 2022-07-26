289aa26f2c70a75e9e0b9da317345f9c8657359d
nevvrmbk <99172207+nevvrmbk@users.noreply.github.com>
2022-07-26 23:03:53 +0300

    Project mod

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,21 @@
 // https://docs.cypress.io/api/table-of-contents
-
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("Home View Test", () => {
+  it("Visit the apps root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    // cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+  });
+});
+
+describe("About View Test", () => {
+  it("Visit the apps root url", () => {
+    cy.visit("/about");
+    // cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+  });
+});
+
+describe("Contact View Test", () => {
+  it("Visit the apps root url", () => {
+    cy.visit("/contact");
+    // cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
   });
 });
